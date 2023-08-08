65b484a40708c5d1657d2335ae13d384cace78ff
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-08-08 12:04:02 +0200

    Estructura general, fotos, explicaciones en rutas

--- src/routes/bebidas/Bebidas.jsx
+++ src/routes/bebidas/Bebidas.jsx
@@ -1,10 +1,20 @@
 
-
+import drinks from '../../assets/img/foto_homeDrinks.jpeg'
 
 export default function Drinks(){
     return(
         <div>
-            <p>Pagina de Bebidas</p>
+            <img src={ drinks } />
+            <h2>Racó de sabors i aromes</h2>
+            <p>
+            A la nostra cantonada de cava, vins i ratafia, ens endinsem en el fascinant món dels sabors i aromes. 
+            Cada ampolla és un viatge sensorial cuidadosament seleccionat per enriquir els teus moments especials. 
+            Des de les bombolles lleugeres del cava fins a la profunditat dels vins, cada elecció és un testimoni de la nostra passió per l'excel·lència enològica. 
+            La nostra selecció inclou joies locals i tresors internacionals, cadascun amb la seva pròpia història i caràcter. 
+            A més, la ratafia, una beguda autèntica de la regió, afegeix un toc tradicional i únic. El nostre objectiu és elevar les teves experiències culinàries, 
+            permetent-te descobrir i apreciar la diversitat de sabors que el món vitiviní té per oferir. Vine i brinda amb nosaltres, 
+            on la varietat i la qualitat es fonen per celebrar els plaers del paladar.
+            </p>
         </div>
     );
 };
--- src/routes/reposteria/Reposteria.jsx
+++ src/routes/reposteria/Reposteria.jsx
@@ -1,10 +1,19 @@
 
-
+import repost from '../../assets/img/foto_homeRepo.jpeg'
 
 export default function Repost(){
     return(
         <div>
-            <p>Pagina de reposteria</p>
+            <img src={ repost } />
+            <h3>Reposteria</h3>
+            <p>
+            En la nostra pastisseria, la màgia de la reposteria pren vida. Cada dolç és una obra d'art elaborada amb cura i destresa. 
+            Utilitzem ingredients de la més alta qualitat per crear sabors que et transportaran a moments de felicitat. 
+            Des de pastissos personalitzats fins a galetes delicades, la nostra varietat de creacions és tan diversa com 
+            els somnis dels nostres clients. La nostra passió per la reposteria es tradueix en cada detall, des de les textures 
+            fines fins als colors vibrants. Amb un toc modern i un toc de tradició, els nostres postres conviden a tastar i a compartir 
+            moments dolços amb els éssers estimats. La nostra pastisseria és el lloc on els desitjos sucrats es fan realitat.
+            </p>
         </div>
     );
 };
