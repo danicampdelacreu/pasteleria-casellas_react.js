8486be5700f808910a1e070bfd09dde615dd4e55
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-08-18 12:06:54 +0200

    Organizar components

--- src/routes/dias/Dias.jsx
+++ src/routes/dias/Dias.jsx
@@ -1,20 +1,33 @@
 
+import './Dias.css';
+
 import days from "../../assets/img/foto_homeDays.png"
 
-export default function Days(){
-    return(
-        <div>
-            <img src={days} />
-            <h3>Ocasions especials</h3>
-            <p>
-            En les ocasions especials, la nostra creativitat pren volada per crear moments inoblidables. 
-            Amb cada data destacada, donem vida a postres únics que capturen l'esperit de la celebració. 
-            Des de pastissos personalitzats fins a delicades creacions, dediquem atenció meticulosa als 
-            detalls perquè cada dolç sigui un testimoni d'amor i feliç commemoració. Amb l'ús d'ingredients 
-            seleccionats i tècniques artesanals, creem postres que no només són exquisits al paladar, sinó 
-            que també capturen l'essència de l'ocasió. Les nostres creacions fan que aquestes dates especials 
-            siguin encara més memorables i dolces per a tots els que comparteixin aquestes ocasions amb nosaltres.
-            </p>
+
+
+export default function Days() {
+    return (
+        <div className='daysGeneral'>
+            <div className='daysPhoto1'>
+                <img src={days} />
+            </div>
+            <div className='daysExpli'>
+                <h2>Ocasions especials</h2>
+                <p>
+                    En les ocasions especials, la nostra creativitat pren volada per crear moments inoblidables.
+                    Amb cada data destacada, donem vida a postres únics que capturen l'esperit de la celebració.
+                    Des de pastissos personalitzats fins a delicades creacions, dediquem atenció meticulosa als
+                    detalls perquè cada dolç sigui un testimoni d'amor i feliç commemoració. Amb l'ús d'ingredients
+                    seleccionats i tècniques artesanals, creem postres que no només són exquisits al paladar, sinó
+                    que també capturen l'essència de l'ocasió. Les nostres creacions fan que aquestes dates especials
+                    siguin encara més memorables i dolces per a tots els que comparteixin aquestes ocasions amb nosaltres.
+                </p>
+            </div>
+            <div className='daysTypes'>
+                <p> imagen producto</p>
+                <p> imagen producto</p>
+                <p> imagen producto</p>
+            </div>
         </div>
     );
 };
--- src/routes/team/Team.jsx
+++ src/routes/team/Team.jsx
@@ -1,17 +1,22 @@
+import './Team.css';
 
 import team from '../../assets/img/foto_homeTeam.webp'
 
-export default function Team(){
-    return(
+export default function Team() {
+    return (
         <div className='teamGeneral'>
-           <img src= { team } />
-           <h3>Nosaltres</h3>
-           <p>
-           Som la pastisseria de Navàs, un lloc dolç i acollidor on els sabors es converteixen en experiències úniques. 
-           Fundada fa un any per l'Edu i l'Ester, la nostra passió per crear delícies artesanals és el que ens impulsa. 
-           Cada pastís, galeta i postres que surten de la nostra cuina està ple d'amor i dedicació. El nostre objectiu 
-           és endolcir els moments especials de la teva vida amb cada mossegada. Benvinguts a un any de sabor i dolçor a Navàs!
-           </p>
+            <div className='teamPhoto1'>
+                <img src={team} />
+            </div>
+            <div className='teamExpli'>
+                <h2>Nosaltres</h2>
+                <p>
+                    Som la pastisseria de Navàs, un lloc dolç i acollidor on els sabors es converteixen en experiències úniques.
+                    Fundada fa un any per l'Edu i l'Ester, la nostra passió per crear delícies artesanals és el que ens impulsa.
+                    Cada pastís, galeta i postres que surten de la nostra cuina està ple d'amor i dedicació. El nostre objectiu
+                    és endolcir els moments especials de la teva vida amb cada mossegada. Benvinguts a un any de sabor i dolçor a Navàs!
+                </p>
+            </div>
         </div>
     );
 };
