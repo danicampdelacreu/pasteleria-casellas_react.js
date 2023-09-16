ea31f4313d2e0f065e1c3af2bac47545fefee6ab
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-09-16 12:00:29 +0200

    CSS general,pruebas para cliente

--- src/routes/team/Team.jsx
+++ src/routes/team/Team.jsx
@@ -12,7 +12,7 @@ export default function Team() {
                 <h2>Nosaltres</h2>
                 <p>
                     Som la pastisseria de Navàs, un lloc dolç i acollidor on els sabors es converteixen en experiències úniques.
-                    Fundada fa un any per l'Edu i l'Ester, la nostra passió per crear delícies artesanals és el que ens impulsa.
+                    Fundada el 2022 per l'Edu i l'Ester, la nostra passió per crear delícies artesanals és el que ens impulsa.
                     Cada pastís, galeta i postres que surten de la nostra cuina està ple d'amor i dedicació. El nostre objectiu
                     és endolcir els moments especials de la teva vida amb cada mossegada. Benvinguts a un any de sabor i dolçor a Navàs!
                 </p>
