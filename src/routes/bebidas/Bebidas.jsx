9fc97c4612790be22ee134d71d17218280aa4cb7
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-09-29 17:31:23 +0200

    Css -header-Bebidas-Pasteleria JSX - Bebida-pasteleria

--- src/routes/bebidas/Bebidas.jsx
+++ src/routes/bebidas/Bebidas.jsx
@@ -30,59 +30,98 @@ export default function Drinks() {
                 <div className='drinksCava'>
                     <div className='cava1'>
                         <img src={cava} />
-                        <p>Pere Ventura 20 anys</p>
+                        <div className='cava1In'>
+                            <h3>Pere Ventura 20 anys</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
                     <div className='cava2'>
                         <img src={cava} />
-                        <p>Pere Ventura 20 anys</p>
+                        <div className='cava2In'>
+                            <h3>Pere Ventura 20 anys</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
                     <div className='cava3'>
                         <img src={cava} />
-                        <p>Pere Ventura 20 anys</p>
+                        <div className='cava3In'>
+                            <h3>Pere Ventura 20 anys</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
                     <div className='cava4'>
                         <img src={cava} />
-                        <p>Pere Ventura 20 anys</p>
+                        <div className='cava4In'>
+                            <h3>Pere Ventura 20 anys</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
                     <div className='cava5'>
                         <img src={cava} />
-                        <p>Pere Ventura 20 anys</p>
+                        <div className='cava5In'>
+                            <h3>Pere Ventura 20 anys</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
                 </div>
                 <div className='drinksVi'>
                     <div className='vino1'>
                         <img src={vi} />
-                        <p>Vin blanc agulla</p>
+                        <div className='vino1In'>
+                            <h3>Vi negre</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
                     <div className='vino2'>
                         <img src={vi} />
-                        <p>Vin blanc agulla</p>
+                        <div className='vino2In'>
+                            <h3>Vi negre</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
                     <div className='vino3'>
                         <img src={vi} />
-                        <p>Vin blanc agulla</p>
+                        <div className='vino3In'>
+                            <h3>Vi negre</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
                     <div className='vino4'>
                         <img src={vi} />
-                        <p>Vin blanc agulla</p>
+                        <div className='vino4In'>
+                            <h3>Vi negre</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
                     <div className='vino5'>
                         <img src={vi} />
-                        <p>Vin blanc agulla</p>
+                        <div className='vino5In'>
+                            <h3>Vi negre</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
                 </div>
                 <div className='drinksRata'>
                     <div className='rata1'>
                         <img src={ratafia} />
-                        <p>Ratafia Bosch</p>
+                        <div className='rata1In'>
+                            <h3>Ratafia Bosch</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
-                    <div className='rata1'>
+                    <div className='rata2'>
                         <img src={ratafia} />
-                        <p>Ratafia Bosch</p>
+                        <div className='rata2In'>
+                            <h3>Ratafia Bosch</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
-                    <div className='rata1'>
+                    <div className='rata3'>
                         <img src={ratafia} />
-                        <p>Ratafia Bosch</p>
+                        <div className='rata3In'>
+                            <h3>Ratafia Bosch</h3>
+                            <p>Explicacio producte</p>
+                        </div>
                     </div>
                 </div>
 
