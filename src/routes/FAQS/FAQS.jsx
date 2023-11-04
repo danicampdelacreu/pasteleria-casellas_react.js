5859cf095fe77912468a384a20c56149c9a91701
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-11-04 11:22:57 +0100

    FAQS, Link onClick scroll top,

--- src/routes/FAQS/FAQS.jsx
+++ src/routes/FAQS/FAQS.jsx
@@ -6,47 +6,69 @@ export default function FAQS() {
         <div className="FAQS">
             <h3 className='first'>Preguntas Frecuentes - Pastisseria Casellas S.L.</h3>
 
-            <h4>Preguntas Generales:</h4>
-            <p>1. ¿Dónde se encuentra ubicada la pastelería Pastisseria Casellas S.L.?</p>
-            <p>2. ¿Cuáles son los horarios de apertura de la pastelería?</p>
-            <p>3. ¿Ofrecen opciones de entrega a domicilio?</p>
-            <p>4. ¿Qué tipos de productos y pasteles ofrecen en Pastisseria Casellas?</p>
-            <p>5. ¿Tienen opciones de pasteles personalizados o pedidos especiales?</p>
-            <p>6. ¿Cuáles son las opciones de pago disponibles?</p>
-            <p>7. ¿Ofrecen opciones para personas con dietas especiales (sin gluten, sin azúcar, etc.)?</p>
-            <p>8. ¿Puedo reservar productos o pasteles para eventos especiales?</p>
+            <h4 className='first'>Preguntas Generales:</h4>
+            <h5>1. ¿Dónde se encuentra ubicada la pastelería Pastisseria Casellas S.L.?</h5>
+            <p> Plaça Catalunya nº 9 de Navàs</p>
+            <h5>2. ¿Cuáles son los horarios de apertura de la pastelería?</h5>
+            <p>DIVENDRES: 10:00-13:00 // 16:00-19:00</p>
+            <p>DISSABTE i DIUMENGE: 10:00-14:00</p>
+            <h5>3. ¿Ofrecen opciones de entrega a domicilio?</h5>
+            <p>No se ofrece entrega a domicilio</p>
+            <h5>4. ¿Qué tipos de productos y pasteles ofrecen en Pastisseria Casellas?</h5>
+            <p>......</p>
+            <h5>5. ¿Tienen opciones de pasteles personalizados o pedidos especiales?</h5>
+            <p>En el menu se encuentra el enlace "ESPECIALS" donde estan nuestras opciones de pedidos especiales/presonalizados</p>
+            <h5>6. ¿Cuáles son las opciones de pago disponibles?</h5>
+            <p>El pago sera en el mismo establecimento y sera efectio o tarejta </p>
+            <h5>7. ¿Ofrecen opciones para personas con dietas especiales (sin gluten, sin azúcar, etc.)?</h5>
+            <p>.......</p>
+            <h5>8. ¿Puedo reservar productos o pasteles para eventos especiales?</h5>
+            <p>Siempre que se reserve con 7 dias de antelacion</p>
 
-            <h4>Sobre Pedidos:</h4>
-            <p>9. ¿Cómo puedo realizar un pedido en Pastisseria Casellas?</p>
-            <p>10. ¿Cuál es el tiempo de anticipación recomendado para hacer un pedido?</p>
-            <p>11. ¿Ofrecen descuentos para pedidos grandes o eventos?</p>
-            <p>12. ¿Cuál es el proceso de cancelación o modificación de un pedido?</p>
-            <p>13. ¿Puedo personalizar el diseño de un pastel para una ocasión especial?</p>
-            <p>14. ¿Qué opciones tienen para regalos y envoltura de regalos?</p>
+            <h4 className='first'>Sobre Pedidos:</h4>
+            <h5>9. ¿Cómo puedo realizar un pedido en Pastisseria Casellas?</h5>
+            <p>Rellenando nuestro formulario</p>
+            <h5>10. ¿Cuál es el tiempo de anticipación recomendado para hacer un pedido?</h5>
+            <p>Recomendamos hacerlo con 7 dias de antelacion</p>
+            <h5>11. ¿Ofrecen descuentos para pedidos grandes o eventos?</h5>
+            <p>Al hacer el pedido se valoran los posibles descuentos</p>
+            <h5>12. ¿Cuál es el proceso de cancelación o modificación de un pedido?</h5>
+            <p>Llamando al telefono minimo 4 dias antes de la fecha de entrega </p>
+            <h5>13. ¿Puedo personalizar el diseño de un pastel para una ocasión especial?</h5>
+            <p>Si</p>
+            <h5>14. ¿Qué opciones tienen para regalos y envoltura de regalos?</h5>
+            <p>......</p>
 
-            <h4>Preguntas sobre Productos:</h4>
-            <p>15. ¿Cuáles son los productos más populares de Pastisseria Casellas?</p>
-            <p>16. ¿Ofrecen opciones sin azúcar o bajas en calorías?</p>
-            <p>17. ¿Qué productos recomiendan para bodas o eventos formales?</p>
-            <p>18. ¿Pueden acomodar alergias alimentarias o restricciones dietéticas?</p>
-            <p>19. ¿Tienen opciones veganas o vegetarianas?</p>
+            <h4 className='first'>Preguntas sobre Productos:</h4>
+            <h5>15. ¿Cuáles son los productos más populares de Pastisseria Casellas?</h5>
+            <h5>16. ¿Ofrecen opciones sin azúcar o bajas en calorías?</h5>
+            <h5>17. ¿Qué productos recomiendan para bodas o eventos formales?</h5>
+            <h5>18. ¿Pueden acomodar alergias alimentarias o restricciones dietéticas?</h5>
+            <h5>19. ¿Tienen opciones veganas o vegetarianas?</h5>
 
-            <h4>Sobre el Formulario de Contacto:</h4>
-            <p>20. ¿Cómo puedo contactar a Pastisseria Casellas S.L. a través del formulario de contacto?</p>
-            <p>21. ¿Cuál es el tiempo de respuesta típico para las consultas enviadas a través del formulario?</p>
-            <p>22. ¿Puedo solicitar información adicional o aclaraciones sobre productos a través del formulario?</p>
-            <p>23. ¿Cuál es la información necesaria para realizar un pedido a través del formulario?</p>
+            <h4 className='first'>Sobre el Formulario de Contacto:</h4>
+            <h5>20. ¿Cómo puedo contactar a Pastisseria Casellas S.L. a través del formulario de contacto?</h5>
+            <p>Tienes nuestro formulario de contacto en el pie de esta misma pagina</p>
+            <h5>21. ¿Cuál es el tiempo de respuesta típico para las consultas enviadas a través del formulario?</h5>
+            <p>Se responderan en horario laboral maximo 48h</p>
+            <h5>22. ¿Puedo solicitar información adicional o aclaraciones sobre productos a través del formulario?</h5>
+            <p>Siempre que tenga alguna duda sobre nuestros productos podra pedirnos informacion y estaremos contentos de poder darles la solucion</p>
+            <h5>23. ¿Cuál es la información necesaria para realizar un pedido a través del formulario?</h5>
+            <p>Rellenar los 3 campos que tiene el formulario: Nombre, email y mensaje. Junto a la aceptacion de la política de privacidad</p>
 
-            <h4>Información de Contacto:</h4>
-            <p>24. ¿Cuál es la dirección de correo electrónico de contacto de Pastisseria Casellas?</p>
-            <p>25. ¿Cuál es el número de teléfono de contacto?</p>
-            <p className='lastP'>26. ¿Tienen perfiles en redes sociales donde pueda seguir sus últimas novedades?</p>
+            <h4 className='first'>Información de Contacto:</h4>
+            <h5>24. ¿Cuál es la dirección de correo electrónico de contacto de Pastisseria Casellas?</h5>
+            <p>pastisseriacasellas@gmail.com</p>
+            <h5>25. ¿Cuál es el número de teléfono de contacto?</h5>
+            <p>+34 621 199 127</p>
+            <h5>26. ¿Tienen perfiles en redes sociales donde pueda seguir sus últimas novedades?</h5>
+            <p>Estamos trabajando en ello. De momento solo tenemos la pagina web</p>
 
             <h3 className='first'>Política de Cookies - Pastisseria Casellas S.L.</h3>
 
-            <p><strong>Última actualización:</strong> [2 de Noviembre de 2023]</p>
+            <p className='first'><strong>Última actualización:</strong> [2 de Noviembre de 2023]</p>
 
-            <h4>1. Introducción</h4>
+            <h4 className='first'>1. Introducción</h4>
             <p>Esta Política de Cookies describe cómo Pastisseria Casellas S.L. utiliza cookies y tecnologías similares para recopilar y almacenar información cuando visitas nuestro sitio web. Esta política explica qué son las cookies, cómo las utilizamos y cómo puedes gestionar tus preferencias de cookies.</p>
 
             <h4>2. ¿Qué son las cookies?</h4>
