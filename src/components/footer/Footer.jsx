ed66677c48ec4cfd2726fd8c6f8abd437f6d2521
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2024-01-23 18:15:23 +0100

    Adjust to Netlify

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
+// import emailjs from '@emailjs/browser';
 import { Link } from 'react-router-dom';
 
 
@@ -12,6 +12,8 @@ export default function Footer() {
     const [checkboxChecked, setCheckboxChecked] = useState(false);
     const form = useRef();
 
+    /* Esta escritura es la correcta para el funcionamiento de envio mails
+    
     const sendEmail = (e) => {
         e.preventDefault();
 
@@ -35,7 +37,7 @@ export default function Footer() {
         } else {
             alert('Por favor, rellena todos los campos y acepta la politica de privacidad');
         }
-    };
+    };*/
 
     const handleCheckboxChange = () => {
         setCheckboxChecked(!checkboxChecked);
@@ -73,10 +75,11 @@ export default function Footer() {
 
 
                 </div>
-
+                {/* Este form es correcto para el funcionamiento de envio mails
+                 <form ref={form} onSubmit={sendEmail}>*/}
                 <div className="footerMensaje">
                     <p> Info: pastisseriacasellas@gmail.com</p>
-                    <form ref={form} onSubmit={sendEmail}>
+                    <form ref={form} >
                         <div className="contacto-name">
                             <input type="text" name="user_name" placeholder='Nom' />
                         </div>
